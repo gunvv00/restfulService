f3677228ddc35eb13822231a68dc3eeef08231c2
gunvv00 <gunvv00@naver.com>
2024-07-30 14:19:19 +0900

    feat : 유저 조회, 생성 api 생성

---
+++ src/main/java/kr/co/myservice/restfulapi/dao/UserDaoService.java
@@ -0,0 +1,53 @@
+package kr.co.myservice.restfulapi.dao;
+
+import kr.co.myservice.restfulapi.bean.User;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+
+    /*
+    * TODO: 이후 DB를 사용하여 조회 및 생성 하는 방식으로 수정 예정
+    * */
+    private static List<User> users = new ArrayList<>();
+
+    private int usersCount = 3;
+
+    static {
+        users.add(new User(1, "Song", new Date()));
+        users.add(new User(2, "Yang", new Date()));
+        users.add(new User(3, "Kim", new Date()));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User save(User user) {
+        // 필수값 초기화
+        if (user.getId() == null) {
+            user.setId(++usersCount);
+        }
+
+        if (user.getJoinDate() == null) {
+            user.setJoinDate(new Date());
+        }
+
+        users.add(user);
+
+        return user;
+    }
+
+    public User findById(int id) {
+        for (User user : users) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+        return null;
+    }
+}

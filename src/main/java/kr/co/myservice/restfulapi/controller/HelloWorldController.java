5ba7c4cca4d3fc5f90ea80cde4bf9ee01deaaba3
gunvv00 <gunvv00@naver.com>
2024-07-31 13:11:22 +0900

    feat : 다국어 처리 기본 로직 및 properties 파일 추가

--- src/main/java/kr/co/myservice/restfulapi/controller/HelloWorldController.java
+++ src/main/java/kr/co/myservice/restfulapi/controller/HelloWorldController.java
@@ -1,12 +1,21 @@
 package kr.co.myservice.restfulapi.controller;
 
 import kr.co.myservice.restfulapi.bean.HelloWorldBean;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -23,5 +32,10 @@ public class HelloWorldController {
         return new HelloWorldBean(String.format("Hello World ,%s", name));
     }
 
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternalized(
+            @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("greeting.message", null, locale);
+    }
 
 }

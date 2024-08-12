c80f393c887e6bc2f8641fd983bb69d7fe431e9c
gunvv00 <gunvv00@naver.com>
2024-08-12 15:27:45 +0900

    feat : Swagger를 이용한 api 명세서 적용

--- src/main/java/kr/co/myservice/restfulapi/bean/User.java
+++ src/main/java/kr/co/myservice/restfulapi/bean/User.java
@@ -3,6 +3,7 @@ package kr.co.myservice.restfulapi.bean;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -13,19 +14,26 @@ import java.util.Date;
 // 명시된 필드에 모든 생성자 생성을 위한 Annotaion
 @AllArgsConstructor
 @JsonIgnoreProperties(value = {"password", "ssn"})
+@Schema(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
+
+    @Schema(title = "사용자 ID", description = "자동 생성 되므로 입력 X")
     private Integer id;
 
+    @Schema(title = "사용자 성함", description = "사용자 이름을 입력합니다.")
     @Size(min = 2, message = "name값은 2글자 이상 입력해주세요.")
     private String name;
 
+    @Schema(title = "등록 일자", description = "사용자의 등록 일자를 입력해주세요.")
     @Past(message = "등록일은 현재 일자를 미래 날짜를 입력 하실수 없습니다.")
     private Date joinDate;
 
 //    @JsonIgnore
+    @Schema(title = "사용자 비밀번호", description = "사용하실 비밀번호를 입력해주세요.")
     private String password;
 
     // 주민번호
 //    @JsonIgnore
+    @Schema(title = "사용자 주민번호", description = "사용자의 주민번호를 입력해주세요. ")
     private String ssn;
 }
---
+++ src/main/java/kr/co/myservice/restfulapi/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package kr.co.myservice.restfulapi.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "RestFul API by Spring Boot 3.x 명세서"
+                , description = "Spring Boot 기반의 RestFul API에 대한 명세서 입니다."
+                , version = "1.0.0"
+        )
+)
+@Configuration
+@RequiredArgsConstructor
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi customTestOpenApi() {
+        // swagger에 공개 하고 싶은 uri만을 설정
+        String[] path = {"/users/**" , "/admin/**"};
+
+        return GroupedOpenApi.builder().group("일반 사용자와 관리자를 위한 USER 도메에 대한 API")
+                .pathsToMatch(path).build();
+    }
+}
